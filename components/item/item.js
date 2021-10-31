ebc7e8968f39e57fdde2fcebf9aa86bae5a0e390
cblackw <blackwc@bgsu.edu>
2021-10-30 21:56:29 -0400

    Using item service for option template

--- components/item/item.js
+++ components/item/item.js
@@ -2,11 +2,55 @@ angular.module('bumbyApp')
     .component('item', {
         templateUrl: 'components/item/item.html',
         bindings: {},
-        controller: ['$routeParams',
-            function ItemController($routeParams) {
+        controller: ['itemData','$routeParams',
+            function ItemController(itemData, $routeParams) {
                 var ctrl = this;
+                ctrl.selectedOptions = {
+                    'colorOption1': {},
+                    'colorOption2': {},
+                    'colorOption3': {},
+                    'colorOption4': {},
+                    'colorOption5': {},
+                    'extras': {
+                        'option1': {
+                            'show': false,
+                            'color': {}
+                        },
+                        'option2': {
+                            'show': false,
+                            'color': {}
+                        },
+                        'option3': {
+                            'show': false,
+                            'color': {}
+                        },
+                        'option4': {
+                            'show': false,
+                            'color': {}
+                        },
+                        'option5': {
+                            'show': false,
+                            'color': {}
+                        }
+                    }
+                };
                 ctrl.itemId = $routeParams.itemId;
                 ctrl.svgUrl = "components/"+ctrl.itemId+"/"+ctrl.itemId+"-svg.html";
+                ctrl.itemData = itemData;
+                ctrl.item = itemData.find(i => i.link === ("/" + ctrl.itemId));
+
+                ctrl.updateOptionColor = function (index, color){
+                    ctrl.selectedOptions["colorOption"+(index + 1)] = color;
+                }
+
+                ctrl.toggleExtraColorShow = function(index, extra) {
+                    extra.show = !extra.show;
+                    ctrl.selectedOptions.extras["option"+(index + 1)].show = extra.show;
+                };
+
+                ctrl.updateExtraColor = function (index, color){
+                    ctrl.selectedOptions.extras["option"+(index + 1)].color = color;
+                }
             }
         ]
     })
